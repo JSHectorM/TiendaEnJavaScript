a5120f8938ecdc6e156e646aec567a13e3d3cfd2
Hector JS <miguel.lin123@gmail.com>
2020-10-04 11:00:19 -0500

    Actividad 3 segunda parte

--- javascript/control.js
+++ javascript/control.js
@@ -5,3 +5,27 @@ function mostrarHoras(){
 }
 
 setInterval(mostrarHoras, 1000);
+
+//---------------------------------
+//Codigo de la consola act 3 parte 2
+
+var precioProducto = 0;
+var cantidadProductos ;
+var validar = false;
+
+
+while(validar == false){
+    cantidadProductos = Number(prompt("¿Cuantos productos son? :")); 
+    if(Number.isInteger(cantidadProductos)){
+        validar=true;
+    }
+    else{
+        console.error('Introduce un numero entero.')
+    }
+}
+
+for(var i = 0; i<cantidadProductos; i++){
+    precioProducto = precioProducto + Number(prompt(`¿Cual es el precio del producto ${i + 1}? :`));
+}
+
+console.log('El precio total es de: $%i2',precioProducto);
