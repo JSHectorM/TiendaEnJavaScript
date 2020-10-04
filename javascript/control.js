034bc3b06c3e10f431c591ce7f950598f82b8028
Hector JS <miguel.lin123@gmail.com>
2020-10-04 11:55:03 -0500

    Formulario estilos y html

--- javascript/control.js
+++ javascript/control.js
@@ -1,31 +1,33 @@
-function mostrarHoras(){
-    var hora = new Date();
-    var parrafo = document.getElementById("fecha");
-    parrafo.innerHTML = hora.getHours() + ":" + hora.getMinutes() + "." + hora.getSeconds();;
+function mostrarHoras() {
+  var hora = new Date();
+  var parrafo = document.getElementById("fecha");
+  parrafo.innerHTML =
+    hora.getHours() + ":" + hora.getMinutes() + "." + hora.getSeconds();
 }
 
 setInterval(mostrarHoras, 1000);
 
 //---------------------------------
 //Codigo de la consola act 3 parte 2
+function cajaReg() {
+  var precioProducto = 0;
+  var cantidadProductos;
+  var validar = false;
 
-var precioProducto = 0;
-var cantidadProductos ;
-var validar = false;
-
-
-while(validar == false){
-    cantidadProductos = Number(prompt("¿Cuantos productos son? :")); 
-    if(Number.isInteger(cantidadProductos)){
-        validar=true;
+  while (validar == false) {
+    cantidadProductos = Number(prompt("¿Cuantos productos son? :"));
+    if (Number.isInteger(cantidadProductos)) {
+      validar = true;
+    } else {
+      console.error("Introduce un numero entero.");
     }
-    else{
-        console.error('Introduce un numero entero.')
-    }
+  }
+
+  for (var i = 0; i < cantidadProductos; i++) {
+    precioProducto =
+      precioProducto +
+      Number(prompt(`¿Cual es el precio del producto ${i + 1}? :`));
+  }
+  console.log("El precio total es de: $%i", precioProducto);
 }
 
-for(var i = 0; i<cantidadProductos; i++){
-    precioProducto = precioProducto + Number(prompt(`¿Cual es el precio del producto ${i + 1}? :`));
-}
-
-console.log('El precio total es de: $%i2',precioProducto);
