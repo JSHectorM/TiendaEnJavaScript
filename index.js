8c8a72688235e109f115a1a7c4fc5397cd43c6e8
Hector JS <miguel.lin123@gmail.com>
2020-10-15 21:54:05 -0500

    Capturas de postman y medos get post delete

--- index.js
+++ index.js
@@ -12,7 +12,17 @@ app.get('/index.html', function (req, res) {
 
 app.get('/formulario.html', function (req, res) {
     res.sendFile(path.resolve(__dirname, 'public/html/formulario.html'));
+    // res.send(200,{products: []});
 });
+
+app.post('/formulario.html', function (req, res) {
+    // console.log(req.body);
+    res.send({messaje: 'El producto se recibido'});
+});
+
+// app.delete('/formulario.html', function (req, res) {
+    
+// });
 
 app.get('/producto.html', function (req, res) {
     res.sendFile(path.resolve(__dirname, 'public/html/producto.html'));
